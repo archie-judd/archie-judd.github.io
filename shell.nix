2d04df181bf2b3ab8ed57f85cd04e94f67bda340
archiejudd <archiejudd@proton.me>
2026-08-22 15:48:21 +0100

    fix: shell.nix

--- shell.nix
+++ shell.nix
@@ -1,57 +1,68 @@
 let
   pkgs = import <nixpkgs> { config.allowUnfree = true; };
-  sandbox = import (fetchTarball
-    "https://github.com/archie-judd/agent-sandbox.nix/archive/main.tar.gz") {
-      pkgs = pkgs;
-    };
+  sandbox =
+    import (fetchTarball "https://github.com/archie-judd/agent-sandbox.nix/archive/main.tar.gz")
+      {
+        pkgs = pkgs;
+      };
   devPackages = [
     pkgs.nodejs
     (pkgs.python3.withPackages (ps: with ps; [ pyyaml ]))
-    pkgs.nodePackages.prettier
+    pkgs.prettier
   ];
   claude-sandboxed = sandbox.mkSandbox {
     pkg = pkgs.claude-code;
     binName = "claude";
-    outName = "claude";
+    outName = "claude-sandboxed";
     allowedPackages = [
       pkgs.coreutils
       pkgs.which
       pkgs.git
+      pkgs.less
       pkgs.ripgrep
       pkgs.fd
       pkgs.gnused
       pkgs.gnugrep
       pkgs.findutils
       pkgs.jq
-    ] ++ devPackages;
-    stateDirs = [ "$HOME/.claude" ];
-    stateFiles = [ "$HOME/.claude.json" "$HOME/.claude.json.lock" ];
-    extraEnv = {
-      # Use literal strings for secrets to evaluate at runtime!
-      # builtins.getEnv will leak your token into the /nix/store.
-      CLAUDE_CODE_OAUTH_TOKEN =
-        "$(${pkgs.coreutils}/bin/cat $HOME/.config/sops-nix/secrets/claude-code-oauth-token)";
+    ]
+    ++ devPackages;
+    rwDirs = [ "$HOME/.claude" ];
+    roFiles = [ "$HOME/.config/git/config" ];
+    env = {
       EDITOR = "nvim";
+      COLORTERM = "truecolor";
+      CLAUDE_CONFIG_DIR = "$CLAUDE_CONFIG_DIR";
+      GH_TOKEN = "$(${pkgs.coreutils}/bin/cat $SOPS_DECRYPTED_DIR/github-read-token)";
+    }
+    // pkgs.lib.optionalAttrs pkgs.stdenv.isDarwin {
+      CLAUDE_CODE_OAUTH_TOKEN = "$(${pkgs.coreutils}/bin/cat $SOPS_DECRYPTED_DIR/claude-code-oauth-token)";
     };
-    restrictNetwork = true;
-    allowedDomains = [
-      # Anthropic
-      "anthropic.com"
-      "claude.com"
-      # GitHub
-      "raw.githubusercontent.com"
-      "api.github.com"
-      # Node
-      "registry.npmjs.org"
-      "nodejs.org"
-    ];
+    allowedDomains = {
+      "anthropic.com" = "*";
+      "claude.com" = "*";
+      "github.com" = "*";
+      "githubusercontent.com" = [
+        "GET"
+        "HEAD"
+      ];
+      "registry.npmjs.org" = [
+        "GET"
+        "HEAD"
+      ];
+      "nodejs.org" = [
+        "GET"
+        "HEAD"
+      ];
+    };
   };
 
-in pkgs.mkShell {
+in
+pkgs.mkShell {
   buildInputs = [
     pkgs.nodejs
     (pkgs.python3.withPackages (ps: with ps; [ pyyaml ]))
-    pkgs.nodePackages.prettier
+    pkgs.prettier
     claude-sandboxed
   ];
 }
